ad277de48a22d0ea3189813dc8550310d0a4f820
soniyakaramchandani42510 <77952055+soniyakaramchandani42510@users.noreply.github.com>
2021-10-28 21:49:12 +0530

    Frontend eachperson (#1)
    * instgram icon and profile card added
    
    * exit button added
    
    * card width and font size changed
    
    * exit button added
    
    Co-authored-by: Soniya Karamchandani <soniyaKaramchandani02@gmail.com>

--- src/components/eachperson.jsx
+++ src/components/eachperson.jsx
@@ -1,26 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
-import Navbar from './navbar';
+import Navbar from "./navbar";
+import Footer from "./footer";
+import "./eachperson.css";
+import "./people-block.css";
+var FontAwesome = require("react-fontawesome");
 
+function EachPerson(props) {
+  return (
+    <div>
+      <Navbar />
+      <button
+        onClick={() => props.ChangeShowUser(null)}
+        className="btn btn-secondary p-3 mb-5 "
+        id="btn1"
+      >
+        <h4>
+          <FontAwesome name="chevron-left" />
+        </h4>
+      </button>
 
-function EachPerson(props){
-    return(
-        <div>
-            <Navbar />
-            <br />
-            <button onClick={() => props.ChangeShowUser(null)}>exit</button>
-            <br />
-            name= {props.user.first_name} {props.user.last_name}
-            <br />
-            email= {props.user.email} 
-            <br />
-            <img src={props.userExtra.profile_photo} alt="profiel" style={{height:'50px'}} />
-            <br />
-            Bio= {props.userExtra.bio} 
-
-            {console.log(props)}
+      {console.log(props)}
+      <div class="user-profile">
+        <img class="avatar" src={props.userExtra.profile_photo} alt="Ash" />
+        <div class="username">
+          {props.user.first_name} {props.user.last_name}
         </div>
-    );
+        <div class="description">{props.user.email}</div>
+        <div class="bio">
+          Batch of 20
+          {parseInt(props.userExtra.batch[3] + props.userExtra.batch[4]) + 4}
+        </div>
+
+        <ul class="data">
+          <li>BIO: &nbsp; {props.userExtra.bio}</li>
+        </ul>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default EachPerson
+export default EachPerson;
